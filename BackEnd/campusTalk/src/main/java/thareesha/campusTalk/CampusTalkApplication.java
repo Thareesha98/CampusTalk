225b135991790f0eb1451fd41183cbc482f242f5
Thareesha <thareesha98>
2025-10-11 11:33:13 +0530

    Initial project commit

---
+++ BackEnd/campusTalk/src/main/java/thareesha/campusTalk/CampusTalkApplication.java
@@ -0,0 +1,13 @@
+package thareesha.campusTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusTalkApplication.class, args);
+	}
+
+}
