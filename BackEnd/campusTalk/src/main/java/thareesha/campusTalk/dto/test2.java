6e710f6ae620e18e1fd22d41017858d5db2d5fd0
Thareesha <thareesha98>
2025-10-13 23:54:51 +0530

    Fix some errors 2

---
+++ BackEnd/campusTalk/src/main/java/thareesha/campusTalk/dto/test2.java
@@ -0,0 +1,148 @@
+package thareesha.campusTalk.dto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import thareesha.campusTalk.model.Club;
+import thareesha.campusTalk.model.Post;
+import thareesha.campusTalk.model.University;
+
+public class test2 {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
+    private String department;
+
+    private String year;
+
+    private String role; // "STUDENT" or "ADMIN"
+
+    private String profilePicUrl;
+    
+    @ManyToMany(mappedBy = "followers")
+    @JsonIgnore
+    private Set<Club> followedClubs = new HashSet<>();
+
+
+    // Relationships
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("user") // avoid recursion in posts
+    private List<Post> posts;
+    
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    private University university;
+
+    
+    public University getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(University university) {
+		this.university = university;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getProfilePicUrl() {
+		return profilePicUrl;
+	}
+
+	public void setProfilePicUrl(String profilePicUrl) {
+		this.profilePicUrl = profilePicUrl;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	public Set<Club> getFollowedClubs() {
+		return followedClubs;
+	}
+
+	public void setFollowedClubs(Set<Club> followedClubs) {
+		this.followedClubs = followedClubs;
+	}
+
+}
