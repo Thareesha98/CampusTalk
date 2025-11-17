e4e651daf574c932a172b1e8c7bbae2e9830f972
Thareesha <thareesha98>
2025-11-17 17:27:42 +0530

    University Details Page initiated

--- BackEnd/campusTalk/src/main/java/thareesha/campusTalk/repository/UniversityRepository.java
+++ BackEnd/campusTalk/src/main/java/thareesha/campusTalk/repository/UniversityRepository.java
@@ -11,10 +11,12 @@ import java.util.Optional;
 public interface UniversityRepository extends JpaRepository<University, Long> {
     Optional<University> findByName(String name);
     
-    @Query("SELECT DISTINCT u FROM University u " +
-            "LEFT JOIN FETCH u.clubs c " +
-            "LEFT JOIN FETCH u.students s " +
-            "WHERE u.id = :id")
-     Optional<University> fetchFullById(@Param("id") Long id);
+    @Query("""
+    	    SELECT u FROM University u
+    	    LEFT JOIN FETCH u.clubs c
+    	    WHERE u.id = :id
+    	""")
+    	Optional<University> fetchWithClubs(@Param("id") Long id);
+
 
 }
